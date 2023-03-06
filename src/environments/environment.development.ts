e77def015c7a5d03e22d695c340ec3e17a4ee44b
Ruan Vinícius <contatoruanvinicius@gmail.com>
2023-03-06 20:43:49 -0300

    Auto-generated commit [ci skip]

--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  apiKey: 'OnyU8mnyzUvT3RnwTPhMFFuyJdf7mG4y6Jn3fSj6',
-  url: `https://api.nasa.gov/planetary/apod?api_key=OnyU8mnyzUvT3RnwTPhMFFuyJdf7mG4y6Jn3fSj6`,
-};
